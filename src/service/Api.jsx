6ae1996cc9a7e9faad1ebd1b1528ba8bdb970186
Igor0501 <s.igor0501@gmail.com>
2023-07-21 02:02:03 +0300

    api

--- src/service/Api.jsx
+++ src/service/Api.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_KEY = 'a4e0e6c94492c515df52f4a6ebcc54c7';
+const API_KEY = 'e3be34e5b80f0c7c351efd5d5ef7e586';
 axios.defaults.baseURL = 'https://api.themoviedb.org/3';
 
 const params = {
